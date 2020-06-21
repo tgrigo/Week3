2d2dcb013f02f9da62ce6dc9dc281a4d04bd7777
Todd G <65108263+tgrigo@users.noreply.github.com>
2020-06-21 20:11:16 +0800

    Add files via upload

--- MegaMenu.sh
+++ MegaMenu.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-####
-chmod a+x ~/scripts/portfolio/Week2/passwordCheck.sh
-chmod a+x ~/scripts/portfolio/Week2/foldermaker.sh
-chmod a+x ~/scripts/portfolio/Week2/setPassword.sh
-chmod a+x ~/scripts/portfolio/Week2/foldercopier.sh
-chmod a+x ~/scripts/portfolio/Week3/calculator.sh
-chmod a+x ~/scripts/portfolio/Week3/megafoldermaker.sh
-chmod a+x ~/scripts/portfolio/Week3/filenames.sh
-chmod a+x ~/scripts/portfolio/Week3/webDownloader.sh
+
+# Set execute permissions for child scripts
+chmod a+x ~/student/scripts/portfolio/Week2/passwordCheck.sh
+chmod a+x ~/student/scripts/portfolio/Week2/foldermaker.sh
+chmod a+x ~/student/scripts/portfolio/Week2/setPassword.sh
+chmod a+x ~/student/scripts/portfolio/Week2/foldercopier.sh
+chmod a+x ~/student/scripts/portfolio/Week3/calculator.sh
+chmod a+x ~/student/scripts/portfolio/Week3/megafoldermaker.sh
+chmod a+x ~/student/scripts/portfolio/Week3/filenames.sh
+chmod a+x ~/student/scripts/portfolio/Week3/webDownloader.sh
      
-# Some colours
+# Some colour variables
 RED="\033[0;31m"
 BRed="\033[1;31m"
 BBlu="\033[1;34m"
@@ -23,14 +24,17 @@ BGREEN='\033[1;32m'
 BLUEONWHTE='\033[34;47m'
 NC="\033[0m"
 
+# Clear terminal
+clear
+
 echo 
 echo -e "${BLCKONWHTE} Hello and welcome to my improved menu page, first we need you to enter your password: ${NC}"
-################### Intial password is 1234   ##############################################
+################### Intial default password is 1234   ##############################################
 
 # the two pipes exits the parent script if the child script returns a false return value
 # the parent script is initiated by the following command
 
-~/scripts/portfolio/Week2/passwordCheck.sh || exit 1
+~/student/scripts/portfolio/Week2/passwordCheck.sh || exit 1
 
 echo -e "${BBlu}Please select what you would like to do from the below menu: ${NC}"
 
@@ -42,52 +46,62 @@ case $os in
 
 # The Create Folder case is used here
     "Create Folder")
-    bash ~/scripts/portfolio/Week2/foldermaker.sh
+    ~/student/scripts/portfolio/Week2/foldermaker.sh
     # exit 0
     ;;
+
 # The Copy Folder case is used here
     "Copy Folder")
-    bash ~/scripts/portfolio/Week2/foldercopier.sh
+    ~/student/scripts/portfolio/Week2/foldercopier.sh
     # exit 0
     ;;
+
 # The Set New Pssword case is used here
     "Set New Password")
-    bash ~/scripts/portfolio/Week2/setPassword.sh
+    ~/student/scripts/portfolio/Week2/setPassword.sh
     # exit 0
     ;;
+
 # The Calculator case is used here
     "Calculator")
-    bash ~/scripts/portfolio/Week3/calculator.sh
+    ~/student/scripts/portfolio/Week3/calculator.sh
     # exit 0
     ;;
+
 # The Create Weekly Folders case is used here
     "Create Weekly Folders")
-    bash ~/scripts/portfolio/Week3/megafoldermaker.sh
+    ~/student/scripts/portfolio/Week3/megafoldermaker.sh
     # exit 0
     ;;
+
 # The Check File Names case is used here
     "Check File Names")
-    bash ~/scripts/portfolio/Week3/filenames.sh
+    ~/student/scripts/portfolio/Week3/filenames.sh
     # exit 0
     ;;
+
 # The Download A File case is used here
     "Download A File")
-    bash ~/scripts/portfolio/Week3/webDownloader.sh
+    ~/student/scripts/portfolio/Week3/webDownloader.sh
     # exit 0
     ;;
+
 # invalid data
     *)
     echo -e "${RED}Sorry but thats an invalid entry, try again.${NC}"
     # exit 1
     ;;
+
 esac
 
-read -p "$(echo -e ${BBlu}Press ${NC})$(echo -e ${BRed}enter ${NC})$(echo -e ${BBlu}to make another selection or type${NC})$(echo -e ${BRed} exit ${NC})$(echo -e ${BBlu}to exit: ${NC})" exit;
+    read -p "$(echo -e ${BBlu}Press ${NC})$(echo -e ${BRed}enter ${NC})$(echo -e ${BBlu}to make another selection or type${NC})$(echo -e ${BRed} exit ${NC})$(echo -e ${BBlu}to exit: ${NC})" exit;
 
 if [ "$exit" = "exit" ]; then
     echo -e "${BLCKONWHTE}Goodbye and have a nice day${NC}" && exit 0
     echo
+
 else
+
 echo "1) Create Folder
 2) Copy Folder
 3) Set New Password
@@ -97,4 +111,7 @@ echo "1) Create Folder
 7) Download A File"
 
 fi
+
 done
+
+exit 0
